b4401ef72dbe9eb140790a72c1c3ae896593c5cf
juanmesa2097 <juanmesa2097@gmail.com>
2021-04-21 14:37:28 -0500

    chore: install & configure NGXS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '@environments/environment';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
 import { CoreModule } from './@core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +11,19 @@ import { LayoutModule } from './components/layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    LayoutModule,
+    NgxsModule.forRoot([], { developmentMode: !environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      disabled: environment.production,
+    }),
+    NgxsLoggerPluginModule.forRoot({
+      disabled: environment.production,
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
